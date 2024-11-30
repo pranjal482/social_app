fa871c8f4cd5b00303c95c2e0de1ca8f9e37c191
pranjal482 <ipisc0051@pandoconsultants.com>
2024-11-30 18:41:33 +0530

    stylesheet added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,10 @@
-import { Stack } from "expo-router";
+import { Slot,Stack } from "expo-router";
+import { Text } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false}} />
+    </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,35 @@
-import { Text, View } from "react-native";
+import { ImageBackground, StyleSheet,Text, View } from "react-native";
+import {Link} from 'expo-router'
+import { StatusBar } from "expo-status-bar";
 
 export default function Index() {
   return (
+    <ImageBackground style={styles.bgImage} source={{uri : "https://imgcdn.stablediffusionweb.com/2024/9/16/7832a91e-5594-4c8b-8ebf-c7e9e1e0234c.jpg"}}>
+    
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Aora</Text>
+        <Text style={{color: 'white', fontSize: '44px', fontWeight: '900'}}>Aora</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{color: 'blue'}}>Go to Profile</Link>
     </View>
+      </ImageBackground>
   );
 }
+
+const styles = StyleSheet.create({
+  container : {
+    display: 'flex',
+    flex: 1,
+   justifyContent: 'center',
+   alignItems: 'center',
+    padding: 20,
+
+  },
+  bgImage : {
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    height: '100%'
+  }
+})
