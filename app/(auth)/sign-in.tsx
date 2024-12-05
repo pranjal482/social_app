813f709fb52e01d0f81043faecd0d1a5869579e5
pranjal482 <ipisc0051@pandoconsultants.com>
2024-12-05 19:05:56 +0530

    sign-in half initiated

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,46 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Image } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import tw from 'twrnc';
+
+import { images } from '../../constants';
+import FormField from '@/components/FormField';
+import CustomButton from '@/components/CustomButton';
 
 const SignIn = () => {
+
+  const [form , setForm] = useState({
+    email: "",
+    password: ''
+  })
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView style={tw`bg-[#161622] h-full`}>
+      <ScrollView>
+        <View style={tw`w-full  justify-center min-h-[85vh] px-4 my-6`}>
+          <Image source={images.logo} resizeMode='contain' style={tw`w-[115px] h-[35px]`} />
+
+          <Text style={tw`text-2xl text-white mt-10 font-semibold`}>Log in to Aora</Text>
+
+          <FormField 
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({...form, email: e})}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+            />
+
+          <FormField 
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({...form, password: e})}
+            otherStyles="mt-7"
+            />
+
+            <CustomButton  />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
---
+++ components/FormField.tsx
@@ -0,0 +1,35 @@
+import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native'
+import React, { useState } from 'react'
+import tw from 'twrnc';
+
+import {icons} from '../constants' ;
+
+const FormField = ({title, value, placeholder, handleChangeText, otherStyles, ...props}) => {
+
+    const [showPassword, setShowPassword] = useState(false)
+
+  return (
+    <View style={tw`space-y-2 ${otherStyles}`}>
+      <Text style={tw`text-base text-gray-100 font-medium mb-2`}>{title}</Text>
+
+      <View style={tw`border-2 border-black-100 w-full h-16 px-4 bg-[#1E1E2D]  rounded-2xl focus:border-[#FF9C01] items-center flex-row`}>
+            <TextInput
+             style={tw`flex-1 text-white font-semibold text-base`}
+             value={value}
+             placeholder={placeholder}
+             placeholderTextColor="#7b7b8b"
+             onChange={handleChangeText}
+             secureTextEntry={title === 'Password' && !showPassword}
+            />
+
+            {title === 'Password' && (
+                <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+                    <Image source={!showPassword ? icons.eye : icons.eyeHide} style={tw`w-6 h-6`} />
+                </TouchableOpacity>
+            )}
+      </View>
+    </View>
+  )
+}
+
+export default FormField
